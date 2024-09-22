9ac1f23735b224d5052817a4603e126350e2308d
Shreyas Udaya <shreyasudaya03@gmail.com>
2024-09-22 09:10:23 +0530

    transpiler interface start

---
+++ modules/gdscript/gdscript_transpiler.cpp
@@ -0,0 +1,13 @@
+#include "gdscript_transpiler.h"
+
+#include "core/error/error_macros.h"
+#include "core/string/char_utils.h"
+
+#ifdef DEBUG_ENABLED
+#include "servers/text_server.h"
+#endif
+
+#ifdef TOOLS_ENABLED
+#include "editor/editor_settings.h"
+#endif
+
